1dff50e7ff58e547759e3a31a49b72c1276d3fb4
tva050 <trym755@gmail.com>
2025-02-27 13:55:46 +0100

    jaaa

--- Data processing/Code/test/test_ploting.py
+++ Data processing/Code/test/test_ploting.py
@@ -6,40 +6,10 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
-path = r"C:\Users\trym7\OneDrive - UiT Office 365\skole\MASTER\Data processing\Data\SMOS\2022\dec_mean_thickness.nc"
-
-def get_data(path):
-    data = nc.Dataset(path)
-    
-    lat = data.variables['latitude'][:]
-    lon = data.variables['longitude'][:]
-    si_thickness = data.variables['sea_ice_thickness'][:]
-    
-    return lat, lon, si_thickness
-
-def plot(lat, lon, si_thickness):
-        # Check if lon and lat are already 2D
-    if lon.ndim == 1 and lat.ndim == 1:
-        lon, lat = np.meshgrid(lon, lat)
-    
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(1, 1, 1, projection=ccrs.NorthPolarStereo())
-    ax.set_extent([-3e6, 3e6, -3e6, 3e6], crs=ccrs.NorthPolarStereo())
-
-    # Plot the sea ice thickness
-    mesh = ax.pcolormesh(lon, lat, si_thickness[0, :, :], transform=ccrs.PlateCarree(), cmap='viridis', vmin=0, vmax=1, zorder=1)
-    
-    ax.add_feature(cfeature.LAND, color='lightgray', alpha=1, zorder=2)
-    ax.add_feature(cfeature.LAKES, edgecolor='gray', facecolor="white", linewidth=0.5, alpha=0.5, zorder=3)
-    ax.add_feature(cfeature.COASTLINE, color = "gray", linewidth=0.5, zorder=4)
-    
-    
-    cbar = plt.colorbar(mesh, orientation='vertical')
-    cbar.set_label('Sea Ice Thickness (m)')
-    plt.title('Sea Ice Thickness')
-    plt.show()
-    
-    
-lat, lon, si_thickness = get_data(path)
-
-plot(lat, lon, si_thickness)
+import numpy as np
+size = 206748
+side_length = int(np.sqrt(size))
+while size % side_length != 0:
+    side_length -= 1
+grid_shape = (side_length, size // side_length)
+print(f"Calculated grid shape: {grid_shape}")
