4e083221d265a0bd6fdfeb79a5cc7192c43404c0
tva050 <trym755@gmail.com>
2025-03-27 15:12:28 +0100

    new validation analysis, mooring data python code established

---
+++ Data processing/Code/Validation code/beaufort_gyre_mooring.py
@@ -0,0 +1,12 @@
+import numpy as np
+from scipy.spatial import cKDTree
+import matplotlib.pyplot as plt
+from cartopy import crs as ccrs, feature as cfeature
+import netCDF4 as nc
+from  scipy.interpolate import griddata
+import pandas as pd
+from pyproj import Proj, Transformer, transform
+import cartopy.crs as ccrs
+import seaborn as sns
+from mpl_toolkits.basemap import Basemap
+
